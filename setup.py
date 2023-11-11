3e2c6c360c876d11aa79b15bbb78f6e8d4038d1d
KEERTHIRAJ HOSADODDI SHIVAMADHU <150257563+hskeerthiraj@users.noreply.github.com>
2023-11-11 16:23:13 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='dsss_homework_2_23415743'
-    version='2.42.0.windows.2',
+    version='2.42.0',
     packages=find_packages(),
     install_requires=[
       'numpy'
