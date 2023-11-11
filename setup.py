476ef8f551ba15c507b743959875721aa4e61e57
KEERTHIRAJ HOSADODDI SHIVAMADHU <150257563+hskeerthiraj@users.noreply.github.com>
2023-11-11 14:24:19 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Math Quiz',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        # list your dependencies here
+    ],
+)
