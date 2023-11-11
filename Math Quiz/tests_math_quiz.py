4bda1d4a5f1654e8ba61a26a570f5061c54f3987
KEERTHIRAJ HOSADODDI SHIVAMADHU <150257563+hskeerthiraj@users.noreply.github.com>
2023-11-11 13:07:03 +0100

    Update tests_math_quiz.py

--- Math Quiz/tests_math_quiz.py
+++ Math Quiz/tests_math_quiz.py
@@ -1,30 +1,39 @@
 import unittest
-from math_quiz import function_A, function_B, function_C
+from math_quiz import generate_random_integer, generate_random_operator, calculate_expression
 
+class TestMathQuiz(unittest.TestCase):
 
-class TestMathGame(unittest.TestCase):
-
-    def test_function_A(self):
-        # Test if random numbers generated are within the specified range
+    def test_generate_random_integer(self):
+        # Test if random integers generated are within the specified range
         min_val = 1
         max_val = 10
         for _ in range(1000):  # Test a large number of random values
-            rand_num = function_A(min_val, max_val)
+            rand_num = generate_random_integer(min_val, max_val)
             self.assertTrue(min_val <= rand_num <= max_val)
 
-    def test_function_B(self):
-        # TODO
-        pass
+    def test_generate_random_operator(self):
+        # Test the generation of random operators
+        operators = set()
+        for _ in range(1000):  # Test a large number of random values
+            operator = generate_random_operator()
+            operators.add(operator)
+            self.assertIn(operator, ['+', '-', '*'])
 
-    def test_function_C(self):
-            test_cases = [
-                (5, 2, '+', '5 + 2', 7),
-                ''' TODO add more test cases here '''
-            ]
+        # Ensure that all three operators are generated at least once
+        self.assertEqual(len(operators), 3)
 
-            for num1, num2, operator, expected_problem, expected_answer in test_cases:
-                # TODO
-                pass
+    def test_calculate_expression(self):
+        # Test the calculation of expressions
+        test_cases = [
+            (5, 2, '+', 7),
+            (8, 3, '*', 24),
+            (10, 2, '-', 8),
+            # Add more test cases as needed
+        ]
+
+        for num1, num2, operator, expected_result in test_cases:
+            result = calculate_expression(num1, num2, operator)
+            self.assertEqual(result, expected_result)
 
 if __name__ == "__main__":
     unittest.main()
